d05e444352cce49e6689d711efa7fbbce142a3c2
zhipenglin <sunandmoon001@163.com>
2024-10-10 17:56:54 +0800

    初始化项目

---
+++ doc/base.js
@@ -0,0 +1,9 @@
+const {default:SuperSelect} = _SuperSelect;
+
+const BaseExample = ()=>{
+    return <div>
+        <SuperSelect />
+    </div>;
+};
+
+render(<BaseExample />);
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import style from './style.module.scss';
+
+const SuperSelect = ()=>{
+    return <span className={style['tips']}>我是一个初始化组件</span>
+};
+
+export default SuperSelect;
