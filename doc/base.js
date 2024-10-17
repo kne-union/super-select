807334c35e61bf3006d43d09c10b591e03e63c96
zhipenglin <sunandmoon001@163.com>
2024-10-17 14:04:56 +0800

    修改已知bug

--- doc/base.js
+++ doc/base.js
@@ -17,7 +17,7 @@ const BaseExample = () => {
     <SuperSelect allowSelectedAll options={optionList} isPopup={false} getSearchCallback={(searchText, item) => {
       return item.label.indexOf(searchText) > -1;
     }} />
-    <SuperSelect allowSelectedAll api={{
+    <SuperSelect api={{
       data: {}, loader: ({ data }) => {
         const { searchText } = data;
         if (!searchText) {
@@ -31,7 +31,7 @@ const BaseExample = () => {
         };
       }
     }} isPopup={false} getSearchProps={(searchText) => {
-      return { data: { searchText } };
+      return { searchText };
     }} />
   </Space>;
 };
